7cc270c8b6f5009e641022f1ef36320510b9d0ea
jvxz1 <saulpool2305@gmail.com>
2024-11-13 11:24:51 -0600

    #

--- to-do.sql
+++ to-do.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-11-2024 a las 15:31:46
+-- Tiempo de generación: 13-11-2024 a las 18:21:52
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `todo`
+-- Base de datos: `to-do`
 --
 
 -- --------------------------------------------------------
