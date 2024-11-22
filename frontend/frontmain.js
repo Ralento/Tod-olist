699e8d13d63e5059c59dd7d3975a25fa423c1e8b
Filis <ricardoembriz4@gmail.com>
2024-11-22 01:28:15 -0600

    alv ya nose q escribir

---
+++ frontend/frontmain.js
@@ -0,0 +1,11 @@
+const menuIcono = document.getElementById('menu-icono');
+const barraLateral = document.getElementById('barra-lateral');
+const cerrarLateral = document.getElementById('cerrar-lateral');
+
+menuIcono.addEventListener('click', () => {
+  barraLateral.classList.add('mostrar');
+});
+
+cerrarLateral.addEventListener('click', () => {
+  barraLateral.classList.remove('mostrar');
+});
